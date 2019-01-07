6a8cc614615328542fbc582c43029a5027999562
JeniaZalomsky <46301889+JeniaZalomsky@users.noreply.github.com>
2019-01-07 20:21:31 +0200

    Update StudentsFactory.java

--- Students_Table/src/students/StudentsFactory.java
+++ Students_Table/src/students/StudentsFactory.java
@@ -2,6 +2,7 @@ package students;
 
 public class StudentsFactory {
 	public ElementCollection<Student, String> createStudents() {
-		return new StudentsDifferentImplimentation();
+		//return new StudentsDifferentImplimentation();
+		return new StudentsSortedImpl();
 	}
 }
